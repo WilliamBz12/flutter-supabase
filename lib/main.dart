6150047603b737a6df0aa13bed20dabea3daa384
Williambz12 <williamsilva754@gmail.com>
2025-04-23 18:14:19 -0300

    video 2.1

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,14 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:sqlite_offline/config/supabase.dart';
 
 import 'config/dependencies.dart';
 import 'ui/home/widgets/home_screen.dart';
 
-void main() {
+void main() async {
+  await SupabaseConfig.init();
+
   runApp(
     MultiProvider(
       providers: providersLocal,
